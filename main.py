77bcb981b1e02e71d7db8e2c572abb36b54effcf
regar0th <50736501+Vinifeju@users.noreply.github.com>
2024-03-05 17:47:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
+from asyncio import run
 from aiogram import Bot, Dispatcher
 
 from command_handler import chgptrouter
 
 from config import Config
-from asyncio import run
+
 
 bot = Bot(token=Config.BOT_TOKEN)
 dp = Dispatcher()
@@ -13,6 +13,7 @@ dp = Dispatcher()
 async def main():
     dp.include_routers(*[chgptrouter])
     await dp.start_polling(bot, skip_updates=True)
+
 
 if __name__ == '__main__':
     run(main())
